3ae47a2f8d7b683199fa8860c15a11d2d380391f
Dave <dave.ollerhead1989@gmail.com>
2021-01-22 16:15:11 +0000

    removed setters on Game class

--- Api/DatascopeTest/Models/Game.cs
+++ Api/DatascopeTest/Models/Game.cs
@@ -1,25 +1,17 @@
 ﻿using System;
-using System.Dynamic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using DatascopeTest.Commands;
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using Microsoft.Net.Http.Headers;
 
 namespace DatascopeTest.Models
 {
     public class Game : Entity
     {
         public int Id { get; private set; }
-        public string Name { get; private set; }
-        public string Description { get; private set; }
-        public DateTime ReleasedAt { get; private set; }
+        public string Name { get; }
+        public string Description { get; }
+        public DateTime ReleasedAt { get; }
         public DateTime CreatedAt { get; }
-        public byte Rating { get; private set; }
+        public byte Rating { get; }
 
-        private Game(string name, string description, DateTime releasedAt, byte rating)
+        public Game(string name, string description, DateTime releasedAt, byte rating)
         {
             Name = name;
             Description = description;
