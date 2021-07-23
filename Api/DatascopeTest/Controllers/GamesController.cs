e422504f23fcf86b31ea73c5e3edc7e2b1d8eee1
Dave Ollerhead <david.ollerhead@datascopeplc.com>
2021-07-23 22:57:05 +0100

    feature/1 comments

--- Api/DatascopeTest/Controllers/GamesController.cs
+++ Api/DatascopeTest/Controllers/GamesController.cs
@@ -12,7 +12,7 @@ namespace DatascopeTest.Controllers
     [Route("[controller]")]
     public class GamesController : ControllerBase
     {
-        // adding a comment... more comments
+        // adding a comment... more comments .//
         private readonly IMediator _mediator;
 
         public GamesController(IMediator mediator)
